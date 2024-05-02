7c9d71bac12f4270d63494725f6bd9dd688240a7
Maqsudkhan <maqsudtorayev2003@gmail.com>
2024-05-02 14:43:50 +0500

    add forgot password

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,15 @@ import { RouterProtectGuard } from './guards/Admin/admin.guard';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { OverwiewComponent } from './components/pages/overwiew/overwiew.component';
 import { RegisterComponent } from './components/register/register.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { ConiformPasswordComponent } from './components/coniform-password/coniform-password.component';
 
 const routes: Routes = [
   { path: 'login', title: 'Login', component: RegisterComponent},
+  { path: 'login', title: 'Login', component: RegisterComponent},
   { path: 'overview', title: 'OverView', component: OverwiewComponent},
+  { path: 'forgot-password', title: 'ForgotPassword', component: ForgotPasswordComponent},
+  { path: 'coniform-password', title: 'ConiformPassword', component: ConiformPasswordComponent},
   { path: '', title: 'OverView', component: OverwiewComponent},
   { path: '**', title: 'Not Found', component: NotFoundComponent},
   
---
+++ src/app/components/coniform-password/coniform-password.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-coniform-password',
+  templateUrl: './coniform-password.component.html',
+  styleUrl: './coniform-password.component.scss'
+})
+export class ConiformPasswordComponent {
+
+}
