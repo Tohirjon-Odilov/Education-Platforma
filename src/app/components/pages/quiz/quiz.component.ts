9e0f7d1ef426d68b1e39b87e5ada95d5d93a9c19
Maqsudkhan <maqsudtorayev2003@gmail.com>
2024-05-03 19:35:12 +0500

    Added complate quiz

--- src/app/components/pages/quiz/quiz.component.ts
+++ src/app/components/pages/quiz/quiz.component.ts
@@ -6,5 +6,46 @@ import { Component } from '@angular/core';
   styleUrl: './quiz.component.scss'
 })
 export class QuizComponent {
-
+  selected: string = "form-item"
+  selected2: string = "form-item"
+  selected3: string = "form-item"
+  selected4: string = "form-item"
+  selected5: string = "form-item"
+  
+  addClass(){
+    this.selected = "form-item form-item-selected"
+    this.selected2 = "form-item"
+    this.selected3 = "form-item"
+    this.selected4 = "form-item"
+    this.selected5 = "form-item"
+    
+  }
+  addClass2(){
+    this.selected2 = "form-item form-item-selected"
+    this.selected = "form-item"
+    this.selected3 = "form-item"
+    this.selected4 = "form-item"
+    this.selected5 = "form-item"
+  }
+  addClass3(){
+    this.selected3 = "form-item form-item-selected"
+    this.selected = "form-item"
+    this.selected2 = "form-item"
+    this.selected4 = "form-item"
+    this.selected5 = "form-item"
+  }
+  addClass4(){
+    this.selected4 = "form-item form-item-selected"
+    this.selected = "form-item"
+    this.selected2 = "form-item"
+    this.selected3 = "form-item"
+    this.selected5 = "form-item"
+  }
+  addClass5(){
+    this.selected5 = "form-item form-item-selected"
+    this.selected = "form-item"
+    this.selected2 = "form-item"
+    this.selected3 = "form-item"
+    this.selected4 = "form-item"
+  }
 }
