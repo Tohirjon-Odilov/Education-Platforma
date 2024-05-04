8166a4536bbf77edca8e5f2ff3d9d28c99ec15e3
Maqsudkhan <maqsudtorayev2003@gmail.com>
2024-05-04 13:08:25 +0500

    Created leaderboard

--- src/app/components/pages/leaderboard/leaderboard.component.ts
+++ src/app/components/pages/leaderboard/leaderboard.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class LeaderboardComponent {
 
+  selected1: string = "Alltime"
+  selected2: string = "Alltime"
+  selected3: string = "Alltime"
+
+  addClass(){
+    this.selected1 = "weekly"
+    this.selected2 = "Alltime"
+    this.selected3 = "Alltime "
+  }
+  addClass2(){
+    this.selected2 = "weekly"
+    this.selected1 = "Alltime"
+    this.selected3 = "Alltime "
+  }
+  addClass3(){
+    this.selected3 = "weekly"
+    this.selected2 = "Alltime"
+    this.selected1 = "Alltime "
+  }
+
+
 }
