7578569fe3465ac641abcbde090a409084c41832
Maqsudkhan <maqsudtorayev2003@gmail.com>
2024-05-04 19:12:34 +0500

    add admin navbar

---
+++ src/app/components/admin/quiz-crud/quiz-crud.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quiz-crud',
+  templateUrl: './quiz-crud.component.html',
+  styleUrl: './quiz-crud.component.scss'
+})
+export class QuizCRUDComponent {
+
+}
