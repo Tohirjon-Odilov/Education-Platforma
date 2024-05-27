4034152033bacea08381219388ef91d5cce7cb56
IConanEdogawa <160710144+IConanEdogawa@users.noreply.github.com>
2024-05-27 11:54:14 +0500

    *added question parts

--- src/app/components/admin/question/question-delete/question-delete.component.ts
+++ src/app/components/admin/question/question-delete/question-delete.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-question-delete',
@@ -7,5 +8,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./question-delete.component.scss']
 })
 export class QuestionDeleteComponent {
+  private apiUrl = 'https://edu-api.tohirjon.uz/api/Question';
+  questions: any[] = [];
+  selectedQuestionId: string | null = null;
+  constructor(private http: HttpClient) { }
 
+  getAllQuestions(): Observable<any> {
+    return this.http.get(`${this.apiUrl}/GetAllQuestions`);
+  }
+
+  deleteQuestion(id: string): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/DeleteQuestion/${id}`);
+  }
+
+
+
+  ngOnInit(): void {
+    this.GetAllQuestions();
+  }
+
+  GetAllQuestions(): void {
+    this.getAllQuestions().subscribe((data: any[]) => {
+      this.questions = data;
+    });
+  }
+
+  DeleteQuestion(): void {
+    if (this.selectedQuestionId) {
+      this.deleteQuestion(this.selectedQuestionId).subscribe(() => {
+        this.getAllQuestions();  // Refresh the list after deletion
+      });
+    }
+  }
 }
--- src/app/components/admin/question/question-get-all/question-get-all.component.ts
+++ src/app/components/admin/question/question-get-all/question-get-all.component.ts
@@ -7,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrl: './question-get-all.component.scss'
 })
 export class QuestionGetAllComponent {
+  questions: any[] = [];
 
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.fetchQuestions();
+  }
+
+  fetchQuestions() {
+    this.http.get<any[]>('https://edu-api.tohirjon.uz/api/Question/GetAllQuestions')
+      .subscribe(
+        (response) => {
+          this.questions = response;
+        },
+        (error) => {
+          console.error('Error fetching questions:', error);
+        }
+      );
+  }
 }
